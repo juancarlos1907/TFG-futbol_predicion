54fb10dae38a685c7888961fb899d53111137a43
Maria del Carmen García García <margargar81@alum.us.es>
2024-05-28 20:53:29 +0200

    Metodo para obtener los partidos

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
@@ -17,9 +19,27 @@ def home():
 
 @app.route('/api/teams', methods=['GET'])
 def get_teams():
-    response = requests.get(f"{API_FOOTBALL_URL}teams", headers=API_FOOTBALL_HEADERS)
-    data = response.json()
-    return jsonify(data)
+    # Lee el archivo CSV
+    teams_df = pd.read_csv('teams.csv')
+
+    # Obtiene el año actual
+    current_year = datetime.now().year
+
+    # Filtra los equipos del año actual -1 porque el listado es hasta 2023 y faltan datos del 24
+    current_teams = teams_df[teams_df['year'] == current_year-1]
+
+    # Selecciona las columnas necesarias
+    teams_info = current_teams[['name', 'logo', 'country']]
+
+    # Convierte el DataFrame a una lista de diccionarios
+    teams_list = teams_info.to_dict(orient='records')
+
+    # Retorna los datos como JSON
+    return jsonify(teams_list)
+
+    # response = requests.get(f"{API_FOOTBALL_URL}teams", headers=API_FOOTBALL_HEADERS)
+    # data = response.json()
+    # return jsonify(data)
 
 @app.route('/api/fixtures', methods=['GET'])
 def get_fixtures():
